32225c2ed15ac51e5d9b5adecf493e2f679ce7c3
KrabbyBuckets <72187815+KrabbyBuckets@users.noreply.github.com>
2021-04-29 13:45:43 -0700

    fix

---
+++ WOKCommands/TS-EXAMPLE/commands/Utils/commandName.ts
@@ -0,0 +1,8 @@
+export default {
+    name: "SOME_COMMAND_NAME",
+    description: "SOME_DESCRIPTION",
+    category: "SOME_CATRGORY",
+    callback: async ({ message, client, args }) => {
+        //do stuff here
+    }
+}
